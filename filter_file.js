2d6ba74beef80e31ecde958b63bb81389711c543
Hao Wu <hwu@intellisurvey.com>
2014-11-26 16:59:07 -0800

    use filter

--- filter_file.js
+++ filter_file.js
@@ -5,11 +5,10 @@ module.exports = function(dir, ext, cb) {
 
 fs.readdir(dir, function doneReading(err, files) { 
     if (err) { return cb(err)  } // early return
-    files.forEach(function (file) {
-       if (path.extname(file) === ext )  {
-        cb(null, file);
-       }
+    var filtered_files = files.filter(function (file) {
+      return path.extname(file) === ext ;
     }   
-    )
+    );
+    cb(null, filtered_files);
 });
 };
--- module_readdir.js
+++ module_readdir.js
@@ -2,7 +2,9 @@ var filter_file = require('./filter_file.js');
 var dir = process.argv[2];
 var ext = '.' + process.argv[3];
 
-filter_file(dir,ext, function(err, file) { 
+filter_file(dir,ext, function(err, files) { 
     if (err) return err;    
+    files.forEach( function(file) {
     console.log(file) 
+    });
 });
