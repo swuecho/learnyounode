f80ccd09e0c46213fd9a76f333eeecfe91389fe3
Hao Wu <hwu@intellisurvey.com>
2014-11-28 13:56:36 -0800

    set header for http response

--- file_server.js
+++ file_server.js
@@ -3,6 +3,8 @@ var fs = require('fs')
 var server = http.createServer(function (req, res) {
     var file = process.argv[3];
     var src = fs.createReadStream(file);
+    // set the header for response
+    res.writeHead(200, { 'content-type': 'text/plain' });
     src.pipe(res);
       // request handling logic...
 })
