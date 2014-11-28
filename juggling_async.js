9befec97a61d2d731a84504e3de486719b91d111
Hao Wu <hwu@intellisurvey.com>
2014-11-28 15:52:06 -0800

    async not working

--- juggling_async.js
+++ juggling_async.js
@@ -1,17 +1,20 @@
  var http = require('http');
  var bl = require('bl');
+ var async = require('async');
  var urls = process.argv.slice(2);
- callback = function(response) {
-     response.pipe(bl(function(err, data) {
-         if (err) {
-             console.log(err);
-             return
-         }
-         var content = data.toString();
-         console.log(content.length);
-         console.log(content);
-     }))
- };
- for (var i = 0; i < urls.length; i++) {
-     http.get(urls[i], callback);
- }
+cb = function (err, res) {
+    console.log(res)
+};
+ async.each(urls, function(url, cb) {
+         http.get(url, function(response) {
+             response.pipe(bl(function(err, data) {
+                 var content = data.toString();
+                 cb(null, content);
+             }));
+         });
+     },
+     function(err ) {
+         if (err) console.log(err);
+     }
+
+ );
