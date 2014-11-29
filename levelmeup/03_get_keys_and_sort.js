2a316013f0f72aeb8b985463b755613f8619c91a
Hao Wu <hwu@intellisurvey.com>
2014-11-29 11:53:57 -0800

    asycn cause problem

--- levelmeup/03_get_keys_and_sort.js
+++ levelmeup/03_get_keys_and_sort.js
@@ -3,15 +3,15 @@ var db = level(process.argv[2]);
 var map = {};
 var keys = [];
 var count = 0;
+
 for (var i = 0; i <= 100; i++) {
     var key = 'key' + i;
+    console.log(i);
     keys.push(key);
-//    console.log(key);
     db.get(key, function(err, value) {
-        // random error
-        // not found
         if (value) {
             console.log(key);
+            console.log(value);
             map[key] = value
         }
         count++; // every time this callback run, count ++
